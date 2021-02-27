3e8a9615b3f12f77c55020e6726a7750086ed66a
Filip Meszaros <meszaros.filip@gmail.com>
2021-02-27 22:01:43 +0100

    Fix AdvancedListener to be more robust and thread-safe

--- src/test/java/org/testng/TestNgAdvancedListenerExample.java
+++ src/test/java/org/testng/TestNgAdvancedListenerExample.java
@@ -13,6 +13,7 @@ import org.selenium.SeleniumConfiguration;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -25,6 +26,8 @@ public class TestNgAdvancedListenerExample extends SeleniumConfiguration impleme
     public ExtentSparkReporter reporter;
     public ExtentReports reports;
     public ExtentTest test;
+    //Make TestListener thread safe, e.g when running testng.xml with multiple threads (method results won't be scrumbled)
+    public ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
 
     final String USER_DIR = System.getProperty("user.dir") + "\\reports\\";
     final String REPORT_SUBDIR = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
@@ -43,56 +46,57 @@ public class TestNgAdvancedListenerExample extends SeleniumConfiguration impleme
         //You can write any additional details to HTML test summary here
         reports.setSystemInfo("Tester", "Filip Meszaros");
         reports.setSystemInfo("Test details", "Write some details here");
-        reports.setSystemInfo("Test details 2", "Write some additional details here");
+
+        //Flush report to create whole report directory needed for screenshots
+        reports.flush();
     }
 
     @Override
     public void onTestStart(ITestResult result) {
         test = reports.createTest(result.getTestClass().getRealClass().getSimpleName() + "." + result.getMethod().getMethodName());
-        reports.flush();
+        extentTest.set(test);
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
         long testDuration = result.getEndMillis() - result.getStartMillis();
-        test.log(Status.PASS, "Test duration: " + TimeUnit.MILLISECONDS.toSeconds(testDuration) + " seconds");
-        reports.flush();
+        extentTest.get().log(Status.PASS, "Test duration: " + TimeUnit.MILLISECONDS.toSeconds(testDuration) + " seconds");
     }
 
     @SneakyThrows
     @Override
     public void onTestFailure(ITestResult result) {
         long testDuration = result.getEndMillis() - result.getStartMillis();
-        test.log(Status.FAIL, "Test duration: " + TimeUnit.MILLISECONDS.toSeconds(testDuration) + " seconds");
-        test.fail(result.getThrowable().getCause());
-        test.fail(result.getThrowable());
+        extentTest.get().log(Status.FAIL, "Test duration: " + TimeUnit.MILLISECONDS.toSeconds(testDuration) + " seconds");
+        extentTest.get().fail(result.getThrowable().getMessage());
+        extentTest.get().fail(result.getThrowable());
 
         //Take screenshot of current screen and append it to report
         File screenshotObject = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        String screenshotName = result.getTestClass().getRealClass().getSimpleName() + "_" + result.getMethod().getMethodName() + ".jpg";
+        String screenshotName = result.getTestClass().getRealClass().getSimpleName() + "_" + result.getMethod().getMethodName() + ".png";
         String absoluteScreenshotPath = REPORT_DIR + "\\" + screenshotName;
         FileUtils.copyFile(screenshotObject, new File(absoluteScreenshotPath));
 
-        test.addScreenCaptureFromPath(absoluteScreenshotPath); //assign screenshot to failed test
-        reports.flush();
+        extentTest.get().addScreenCaptureFromPath(absoluteScreenshotPath); //assign screenshot to failed test
     }
 
     @Override
     public void onTestFailedWithTimeout(ITestResult result) {
-        test.log(Status.FAIL, "Test Failed with timeout");
-        reports.flush();
+        long testDuration = result.getEndMillis() - result.getStartMillis();
+        extentTest.get().log(Status.FAIL, "Test Failed with timeout");
+        extentTest.get().log(Status.FAIL, "Test duration: " + TimeUnit.MILLISECONDS.toSeconds(testDuration) + " seconds");
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-        test.skip(result.getThrowable().getCause());
-        test.skip(result.getThrowable());
-
-        reports.flush();
+        extentTest.get().skip(result.getThrowable().getCause());
+        extentTest.get().skip(result.getThrowable());
     }
 
     @Override
     public void onFinish(ITestContext context) {
+        long runTimeInSeconds = (context.getEndDate().getTime() - context.getStartDate().getTime()) / 1000L;
+        reports.setSystemInfo("Running time", runTimeInSeconds + " seconds");
         reports.flush();
     }
 }
