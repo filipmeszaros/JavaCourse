c7dd827e3f7ea3b96814c935a509f6da3ac04264
Filip Meszaros <meszaros.filip@gmail.com>
2021-03-01 23:33:28 +0100

    Update Strings.java

--- src/main/java/org/syntax/Strings.java
+++ src/main/java/org/syntax/Strings.java
@@ -21,9 +21,14 @@ public class Strings {
         System.out.println("Str1 equals Str2: " + str.equals(str2));
         System.out.println("String is empty: " + str.isEmpty());
         System.out.println("All ! are replaced with ???: " + str.replace("!", "???"));
-        System.out.println("All leading and trailing spaces are trimmer: " + str2.trim());
+        System.out.println("All leading and trailing spaces are trimmed: " + str2.trim());
         System.out.println("Substring starting from index 6 is: " + str.substring(5));
         System.out.println("Substring starting from index 6 and ending with index 10 is: " + str.substring(6, 10));
+        System.out.println("Comparing 2 string lexicographically: " + str.compareTo(str2));
+        System.out.println("Comparing 2 string lexicographically with ignoring case: " + str.compareToIgnoreCase(str2));
+        System.out.println("Concatenating 2 strings: " + str.concat(str2));
+        System.out.println("'" + str + "' matches regular expression [a-zA-Z0-9]+ : " + str.matches("[a-zA-Z0-9]+"));
+        System.out.println("'" + str + "' matches regular expression [a-zA-Z0-9 !]+ : " + str.matches("[a-zA-Z0-9 !]+"));
 
         String string1 = new String("string");     //creates a separate object in memory
         String string2 = new String("string");     //creates a separate object in memory
@@ -32,5 +37,11 @@ public class Strings {
         System.out.println(string1.equals(string2));      //prints true - compares value of string
         System.out.println(string1 == string2);           //prints false - compares references to objects
         System.out.println(string3 == string4);           //prints true - compares references to objects
+
+        System.out.println("---- String.format() examples -----");
+        System.out.println(String.format("Customer '%s' has %d years", "joe", 35));
+        System.out.println(String.format("Constant PI rounded to 2 decimals: %.2f", 3.141492654));
+        System.out.println(String.format("Constant PI rounded to 4 decimals: %.4f", 3.141492654));
+        System.out.println(String.format("Constant PI rounded to 5 decimals: %.5f", 3.141492654));
     }
 }
